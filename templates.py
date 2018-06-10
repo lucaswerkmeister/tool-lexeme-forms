db31f92f4f90f2fec93eb2a8be71a8eaab82933a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-06-10 16:33:31 +0200

    Add german-noun-feminine and german-noun-neuter templates

--- templates.py
+++ templates.py
@@ -68,6 +68,144 @@ templates = {
         },
         'submit': 'Anlegen',
     },
+    'german-noun-feminine': {
+        'label': 'Deutsches Substantiv (Femininum)',
+        'language_item_id': 'Q188',
+        'language_code': 'de',
+        'lexical_category_item_id': 'Q1084',
+        'forms': [
+            {
+                'label': 'Nominativ singular',
+                'example': 'Das ist die [Katze].',
+                'grammatical_features_item_ids': ['Q131105', 'Q110786'],
+            },
+            {
+                'label': 'Genitiv singular',
+                'example': 'Das Eigentum der [Katze].',
+                'grammatical_features_item_ids': ['Q146233', 'Q110786'],
+            },
+            {
+                'label': 'Dativ singular',
+                'example': 'Das gehört der [Katze].',
+                'grammatical_features_item_ids': ['Q145599', 'Q110786'],
+            },
+            {
+                'label': 'Akkusativ singular',
+                'example': 'Ich mag die [Katze].',
+                'grammatical_features_item_ids': ['Q146078', 'Q110786'],
+            },
+            {
+                'label': 'Nominativ plural',
+                'example': 'Das sind die [Katzen].',
+                'grammatical_features_item_ids': ['Q131105', 'Q146786'],
+            },
+            {
+                'label': 'Genitiv plural',
+                'example': 'Das Eigentum der [Katzen].',
+                'grammatical_features_item_ids': ['Q146233', 'Q146786'],
+            },
+            {
+                'label': 'Dativ plural',
+                'example': 'Das gehört den [Katzen].',
+                'grammatical_features_item_ids': ['Q145599', 'Q146786'],
+            },
+            {
+                'label': 'Akkusativ plural',
+                'example': 'Ich mag die [Katzen].',
+                'grammatical_features_item_ids': ['Q146078', 'Q146786'],
+            },
+       ],
+        'claims': {
+            'P5185': [
+                {
+                    'mainsnak': {
+                        'snaktype': 'value',
+                        'property': 'P5185',
+                        'datatype': 'wikibase-item',
+                        'datavalue': {
+                            'type': 'wikibase-entityid',
+                            'value': {
+                                'entity-type': 'item',
+                                'id': 'Q1775415',
+                            },
+                        },
+                    },
+                    'type': 'statement',
+                    'rank': 'normal',
+                }
+            ],
+        },
+        'submit': 'Anlegen',
+    },
+    'german-noun-neuter': {
+        'label': 'Deutsches Substantiv (Neutrum)',
+        'language_item_id': 'Q188',
+        'language_code': 'de',
+        'lexical_category_item_id': 'Q1084',
+        'forms': [
+            {
+                'label': 'Nominativ singular',
+                'example': 'Das ist das [Kind].',
+                'grammatical_features_item_ids': ['Q131105', 'Q110786'],
+            },
+            {
+                'label': 'Genitiv singular',
+                'example': 'Das Eigentum des [Kindes].',
+                'grammatical_features_item_ids': ['Q146233', 'Q110786'],
+            },
+            {
+                'label': 'Dativ singular',
+                'example': 'Das gehört dem [Kind].',
+                'grammatical_features_item_ids': ['Q145599', 'Q110786'],
+            },
+            {
+                'label': 'Akkusativ singular',
+                'example': 'Ich mag das [Kind].',
+                'grammatical_features_item_ids': ['Q146078', 'Q110786'],
+            },
+            {
+                'label': 'Nominativ plural',
+                'example': 'Das sind die [Kinder].',
+                'grammatical_features_item_ids': ['Q131105', 'Q146786'],
+            },
+            {
+                'label': 'Genitiv plural',
+                'example': 'Das Eigentum der [Kinder].',
+                'grammatical_features_item_ids': ['Q146233', 'Q146786'],
+            },
+            {
+                'label': 'Dativ plural',
+                'example': 'Das gehört den [Kindern].',
+                'grammatical_features_item_ids': ['Q145599', 'Q146786'],
+            },
+            {
+                'label': 'Akkusativ plural',
+                'example': 'Ich mag die [Kinder].',
+                'grammatical_features_item_ids': ['Q146078', 'Q146786'],
+            },
+       ],
+        'claims': {
+            'P5185': [
+                {
+                    'mainsnak': {
+                        'snaktype': 'value',
+                        'property': 'P5185',
+                        'datatype': 'wikibase-item',
+                        'datavalue': {
+                            'type': 'wikibase-entityid',
+                            'value': {
+                                'entity-type': 'item',
+                                'id': 'Q1775461',
+                            },
+                        },
+                    },
+                    'type': 'statement',
+                    'rank': 'normal',
+                }
+            ],
+        },
+        'submit': 'Anlegen',
+    },
     'german-noun-masculine-test': {
         'test': True,
         'label': 'Deutsches Substantiv (Maskulinum), test.wikidata.org',
