1e41392fb61b70b7dbb0b8e9743a9c7a4c3b6752
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-15 14:13:03 +0100

    Add test that entities used in templates exist
    This should help detect cases similar to the ones fixed in the previous
    two commits more quickly – if any items mentioned in a template are
    turned into a redirect (or even deleted), a test failure results. I’ll
    also set up Travis CI to run this test periodically even if there are no
    new commits.

---
+++ test_templates.py
@@ -0,0 +1,32 @@
+import mwapi
+
+import templates
+
+def test_entities_exist():
+    entity_ids = set()
+    for template in templates.templates.values():
+        if template.get('test', False):
+            continue
+        entity_ids.add(template['language_item_id'])
+        entity_ids.add(template['lexical_category_item_id'])
+        for form in template['forms']:
+            entity_ids.update(form['grammatical_features_item_ids'])
+        for property_id, statement_group in template.get('claims', {}).items():
+            entity_ids.add(property_id)
+            for statement in statement_group:
+                entity_ids.add(statement['mainsnak']['datavalue']['value']['id'])
+
+    entity_ids = list(entity_ids)
+    session = mwapi.Session('https://www.wikidata.org', user_agent='lexeme-forms test (https://tools.wmflabs.org/lexeme-forms; mail@lucaswerkmeister.de)')
+    missing_entity_ids = set()
+    while entity_ids:
+        chunk, entity_ids = entity_ids[:50], entity_ids[50:]
+        result = session.get(action='wbgetentities',
+                             ids=chunk,
+                             redirects='no',
+                             props=[])
+        for entity_id, entity in result['entities'].items():
+            if 'missing' in entity:
+                missing_entity_ids.add(entity_id)
+
+    assert not missing_entity_ids
