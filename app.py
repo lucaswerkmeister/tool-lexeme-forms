cd4239904a116250c3f67620ea423c373a5f2ac5
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-11-12 00:27:17 +0100

    Work around 'und' language codes in duplicates search
    The search sometimes returns matches with language code 'und' instead of
    the one from the lemma, causing us to reject them as false positives
    instead of duplicate existing lexemes. Treat any 'und' language code as
    equivalent to the one we searched for to avoid this – it’s probably
    better to have some false positives (duplicates that aren’t any) than
    false negatives (undetected duplicates), since false positives can be
    overridden by the user but false negatives directly produce duplicate
    data in Wikidata that needs to be cleaned up afterwards.
    
    Bug: T230833

--- app.py
+++ app.py
@@ -399,7 +399,9 @@ def get_duplicates(wiki, language_code, lemma):
     )
     matches = collections.OrderedDict()
     for result in response['search']:
-        if result['label'] == lemma and result['match']['language'] == language_code:
+        if (result['label'] == lemma and
+            (result['match']['language'] == language_code or
+             result['match']['language'] == 'und')): # T230833
             matches[result['id']] = {'id': result['id'], 'uri': result['concepturi'], 'label': result['label'], 'description': result['description']}
 
     if matches:
