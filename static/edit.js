70dd1521221751dffa47508ea6463919d04cf415
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-05-09 15:57:40 +0200

    Allow drag’n’drop of unmatched forms in edit mode
    The user can now drag a URL from the list of unmatched (or ambiguous, I
    guess) forms into the right input field, and a little JavaScript snippet
    will correctly mangle it so that the backend recognizes it (form ID, not
    full URL). As a bonus, we afterwards remove the dragged list entry as
    well, to prevent accidentally dragging it a second time. Also included
    is a little bit of CSS to make it more obvious where the user can drop
    the links.

---
+++ static/edit.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    let draggedElement = null;
+
+    function dragstart(event) {
+        draggedElement = event.target;
+        document.documentElement.classList.add('dragging')
+    }
+
+    function drop(event) {
+        const url = event.dataTransfer.getData('text/plain');
+        const match = url.match(/Lexeme:(L[1-9][0-9]*)#(F[1-9][0-9]*)$/);
+        if (match === null ) {
+            return;
+        }
+        const [full_match, lexeme_id, form_id] = match;
+        if (event.target.value !== '') {
+            event.target.value += '/';
+        }
+        event.target.value += lexeme_id + '-' + form_id;
+        event.preventDefault();
+
+        const link = draggedElement;
+        const listItem = link.parentElement;
+        const list = listItem.parentElement;
+        const alert = list.parentElement;
+        listItem.remove();
+        if (list.childElementCount === 0) {
+            alert.remove();
+        }
+    }
+
+    function dragend(event) {
+        draggedElement = null;
+        document.documentElement.classList.remove('dragging');
+    }
+
+    window.addEventListener('dragstart', dragstart);
+    document.querySelectorAll('input[name=form_representation]').forEach(element => element.addEventListener('drop', drop));
+    window.addEventListener('dragend', dragend);
+});
