30c561955fd22ca6f8f2122127c359363ccc3123
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-04-14 14:36:03 +0200

    Add login link to navbar
    Rename render_oauth_username() to authentication_area() (the name used
    in other tools of mine) and include a login link if the user is not
    logged in (assuming login is possible at all, i.e. OAuth is configured).
    
    Part of #36.

--- app.py
+++ app.py
@@ -147,10 +147,16 @@ def user_link(user_name):
             flask.Markup(r'</a>'))
 
 @app.template_global()
-def render_oauth_username():
+def authentication_area():
+    if 'oauth' not in app.config:
+        return flask.Markup()
+
     userinfo = get_userinfo()
     if userinfo is None:
-        return flask.Markup(r'')
+        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
+                flask.Markup.escape(flask.url_for('login')) +
+                flask.Markup(r'">Log in</a>'))
+
     return (flask.Markup(r'<span class="navbar-text">Logged in as ') +
             user_link(userinfo['name']) +
             flask.Markup(r'</span>'))
