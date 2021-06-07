dcb35421793fb1e28404875f5da938a205a81f75
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-06-07 21:54:20 +0200

    Add link to create duplicate lexeme in bulk mode results
    When bulk mode refuses to create a lexeme because a potential duplicate
    already exists, add a link to the regular page to create the lexeme
    anyway. (On that page, users will then still have to check the “this is
    not a duplicate” box – I don’t want to add a simple URL parameter to
    pre-check that box because it could be abused, and guarding it with some
    kind of token seems like too much effort.)
    
    The previous “actionable” flag for the duplicates warning is replaced
    with “in_bulk_mode”, since that’s what it means and determines how it’s
    used. If we need more flexibility here later, then we’ll have to rename
    the flag again and/or split it up, but for now this seems like the
    simplest solution. (If there are two flags, and the “instructions” and
    “create” messages can theoretically appear at the same time, then they
    have to be separated somehow, but wrapping them each in a <p> adds
    vertical whitespace…)
    
    Fixes #51.

--- app.py
+++ app.py
@@ -104,11 +104,11 @@ def split_example(form):
         raise Exception('Invalid template: missing [placeholder]: ' + example)
 
 @app.template_filter()
-def render_duplicates(duplicates, actionable, template_name=None, form_representations=[]):
+def render_duplicates(duplicates, in_bulk_mode, template_name=None, form_representations=[]):
     return flask.render_template(
         'duplicates.html',
         duplicates=duplicates,
-        actionable=actionable,
+        in_bulk_mode=in_bulk_mode,
         template_name=template_name,
         form_representations=form_representations,
     )
@@ -618,7 +618,7 @@ def get_duplicates_api(wiki, language_code, lemma):
     if flask.request.accept_mimetypes.accept_html:
         return render_duplicates(
             matches,
-            actionable=True,
+            in_bulk_mode=False,
             template_name=flask.request.args.get('template_name'),
         )
     else:
