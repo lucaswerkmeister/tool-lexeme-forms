661f8e268ac600864a0f6ab9b4869e07bae02f04
Lucas Werkmeister <mail@lucaswerkmeister.de>
2023-04-02 13:19:33 +0200

    Hard-code module order in entity_ids/__main__.py
    The order returned by os.listdir() is arbitrary, and I want the CSS to
    be printed in a well-defined order. But we can keep the directory
    listing for checking that all files are listed and I didn’t forget any.

--- entity_ids/__main__.py
+++ entity_ids/__main__.py
@@ -4,7 +4,18 @@ import subprocess
 import sys
 
 dir = os.path.dirname(os.path.realpath(__file__))
+files = [
+    'property_ids.py',
+    'language_item_ids.py',
+]
+for file in files:
+    subprocess.run([sys.executable, file], cwd=dir, check=True)
+
+missing_files = set()
 for entry in os.listdir(dir):
     if entry.startswith('_'):
         continue
-    subprocess.run([sys.executable, entry], cwd=dir, check=True)
+    if entry not in files:
+        missing_files.add(entry)
+if missing_files:
+    sys.exit(f'You forgot to add these file(s) to entity_ids/__main__.py: {missing_files}')
