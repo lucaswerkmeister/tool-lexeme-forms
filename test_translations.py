3e7f3afd6841a8a313274ef31214d558ed94409c
Lucas Werkmeister <mail@lucaswerkmeister.de>
2024-03-17 14:07:25 +0100

    Generalize more translation tests
    I think the new test_message_formatting() covers most of the checks that
    the old test_message_syntax_*() did, as well as (via the markup in the
    “normal” case) test_link_not_in_gender_logged_in(). Note also that the
    old tests date quite a ways back, with the first one apparently having
    been introduced in bff72090c1, which was back when I was still
    converting the Wikitext syntax to Python syntax manually (which has been
    obsolete since 4bd016e914 and 0a7487b39d), so it stands to reason that
    not all of their checks are relevant anymore.

--- test_translations.py
+++ test_translations.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
-import builtins
+from collections.abc import Callable
+from markupsafe import Markup
 import pytest
 import re
+from typing import Any
 
 from app import translations
-from language import lang_int2babel
 from language_info import autonym
 import toolforge_i18n.formatters as formatters
 import tool_translations_config
@@ -17,21 +18,6 @@ def language_code(request):
 
 @pytest.fixture(scope="module", params=translations['en'].keys())
 def message_key(request):
-    return request.param
-
-
-@pytest.fixture(scope="module", params=['m', 'f', 'n'])
-def gender(request):
-    return request.param
-
-
-@pytest.fixture(scope="module", params=[0, 1, 2, 3, 5, 1000])
-def number(request):
-    return request.param
-
-
-@pytest.fixture(scope="module", params=[[], ["list item 1"], ["list item 1", "list item 2"]])
-def list(request):
     return request.param
 
 
@@ -132,124 +118,44 @@ def test_message_variables(language_code: str, message_key: str):
             assert '{' + variable + '!l}' not in message
 
 
-def test_message_syntax_valid_duplicates_warning(language_code: str, number: int):
-    if 'duplicates-warning' in translations[language_code]:
-        message = translations[language_code]['duplicates-warning']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            num_lexemes=number,
-        )
+def test_message_formatting(language_code: str, message_key: str):
+    """Test that the translation uses variables correctly.
 
+    See the TranslationConfig.variables docstring
+    for the meaning of the different variable names / prefixes."""
+    message = translations[language_code].get(message_key)
+    if message is None:
+        return
 
-def test_message_syntax_valid_duplicates_instructions(language_code: str, number: int):
-    if 'duplicates-instructions' in translations[language_code]:
-        message = translations[language_code]['duplicates-instructions']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            num_lexemes=number,
-        )
+    url = 'https://example.com/test?foo=bar#baz'
+    assert_contains = []
+    get_gender: Callable[[str], str] = unused
+    params: dict[str, Any] = {}
+    for variable in tool_translations_config.config.variables.get(message_key, []):
+        if variable == 'url':
+            params[variable] = url
+            assert_contains.append(url)
+        elif variable == 'user_name':
+            get_gender = lambda user_name: 'n'  # noqa: E731
+            params[variable] = 'User name'
+        elif variable.startswith('num_'):
+            params[variable] = 123
+        elif variable.startswith('list_'):
+            item_1 = Markup('<a href="{url}">{variable} A</a>').format(url=url, variable=variable)
+            item_2 = Markup('<a href="{url}">{variable} B</a>').format(url=url, variable=variable)
+            params[variable] = [item_1, item_2]
+            assert_contains.append(item_1)
+            assert_contains.append(item_2)
+        else:
+            value = Markup('<a href="{url}">{variable}</a>').format(url=url, variable=variable)
+            params[variable] = value
+            assert_contains.append(value)
 
+    formatter = formatters.I18nFormatter(
+        locale_identifier=tool_translations_config.config.language_code_to_babel(language_code),
+        get_gender=get_gender,
+    )
+    formatted = formatter.format(message, **params)
 
-def test_message_syntax_valid_description_with_forms_and_senses(language_code: str, number: int):
-    if 'description-with-forms-and-senses' in translations[language_code]:
-        message = translations[language_code]['description-with-forms-and-senses']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            description='description',
-            num_forms=number,
-            num_senses=number,
-        )
-
-
-def test_message_syntax_valid_edit_ambiguous_warning(language_code: str, number: int):
-    if 'edit-ambiguous-warning' in translations[language_code]:
-        message = translations[language_code]['edit-ambiguous-warning']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            num_forms=number,
-        )
-
-
-def test_message_syntax_valid_edit_unmatched_warning(language_code: str, number: int):
-    if 'edit-unmatched-warning' in translations[language_code]:
-        message = translations[language_code]['edit-unmatched-warning']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            num_forms=number,
-        )
-
-
-def test_message_syntax_valid_edit_form_list_item(language_code: str, list: builtins.list[str], number: int):
-    if 'edit-form-list-item' in translations[language_code]:
-        message = translations[language_code]['edit-form-list-item']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            form_link='',
-            list_grammatical_feature_labels=list,
-            num_statements=number,
-        )
-
-
-def test_message_syntax_valid_bulk_not_allowed(language_code: str, gender: str):
-    if 'bulk-not-allowed' in translations[language_code]:
-        message = translations[language_code]['bulk-not-allowed']
-        formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=lambda value: gender,
-        ).format(
-            message,
-            user_name='some user',
-        )
-
-
-def test_message_syntax_valid_login_hint(language_code: str):
-    if 'login-hint' in translations[language_code]:
-        message = translations[language_code]['login-hint']
-        formatted = formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=unused,
-        ).format(
-            message,
-            url='https://example.com',
-        )
-        assert '<a href=' in formatted
-
-
-def test_link_not_in_gender_logged_in(language_code: str, gender: str):
-    """Assert that a message with both {{GENDER:}} and a link renders correctly.
-
-    Currently, this means that the link must not appear inside the gender spec.
-    If it does, i.e. {{GENDER:$2|...$1...}} in wikitext or {user_name!g:m=...{user_link}...} in Python,
-    then the colon in http:// or https:// in the link will be misinterpreted as a m:f:n separator,
-    resulting in the rest of the link (including the closing tag!) being discarded."""
-    if 'logged-in' in translations[language_code]:
-        user_name = 'some user'
-        user_link = f'<a href="https://example.com/">{user_name}</a>'
-        message = translations[language_code]['logged-in']
-        formatted = formatters.I18nFormatter(
-            locale_identifier=lang_int2babel(language_code),
-            get_gender=lambda value: gender,
-        ).format(
-            message,
-            user_link=user_link,
-            user_name=user_name,
-        )
-        assert user_link in formatted
+    for assertion in assert_contains:
+        assert assertion in formatted
